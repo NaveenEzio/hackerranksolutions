cc3b1bd651895f5d5503e401908399ea37ae0e73
Naveen S <infantnaveen13@gmail.com>
2018-08-14 10:38:50 +0530

    Add files via upload

---
+++ Print Function.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    a=1
+    b=[]
+    for i in range(0,n):
+        b.append(a)
+        a+=1
+        
+print(*b,sep="")        
+        
+        
+    
+    
+    
+    
+
