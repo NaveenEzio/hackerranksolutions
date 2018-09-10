b11b043667399aaa4c1a9aa9685db47fa7d098e0
Naveen S <infantnaveen13@gmail.com>
2018-09-10 15:25:27 +0530

    Update Symmetric Difference.py

--- Symmetric Difference.py
+++ Symmetric Difference.py
@@ -1,13 +1,13 @@
 m=int(raw_input())
-a=set(map(int,raw_input().strip().split(" ")))
+a=set(map(int,raw_input().strip().split(" ")))  #getting input by including space & removing duplicate elements
 n=int(raw_input())
 b=set(map(int,raw_input().strip().split(" ")))
 
      
-e=(a.difference(b))
+e=(a.difference(b))  # Find Difference A & b elements present in a but not in b
 f=(b.difference(a))      
-g=sorted(e.union(f))     
+g=sorted(e.union(f))     #sort using Ascending Order
 
 for i in g:
-    print i
+    print i               #print the sorted elements
       
